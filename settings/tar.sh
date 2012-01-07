ea2c1217678e460f0a5723cfa4bf903fb45d754e
Dmitry Sigaev <www@sigaev.com>
2012-01-06 20:45:15 -0500

    Use a temporary directory when saving settings

--- settings/tar.sh
+++ settings/tar.sh
@@ -1,12 +1,11 @@
-[[ -e $1 ]] && mv $1{,~}
 tar cJC ~ -f "$@"
-[[ -e $1~ ]] || exit 0
+[[ -e `basename $1` ]] || exec mv $1 .
 d=${1%.txz}
 mkdir $d{,~}
 tar xJC $d -f $1
-tar xJC $d~ -f $1~
-diff -ru $d{~,} && mv $1{~,} && exec rm -fr $d{,~}
+tar xJC $d~ -f `basename $1`
+diff -ru $d{~,} && exec rm -fr $d{,~} $1
 echo 'Accept diff (yes/no)?'
 read q
-[[ yes == $q ]] || mv $1{~,}
-exec rm -fr $d{,~} $1~
+[[ yes == $q ]] && mv $1 .
+exec rm -fr $d{,~} $1
